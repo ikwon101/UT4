14d3c97dceb58e8817a1a722b4c187fc5318d801
acer <acer@192.168.1.156>
2021-05-29 17:11:38 +0100

    b.Corregir la tabulación del código y dar formato al código.

--- utilidades/Circulo.java
+++ utilidades/Circulo.java
@@ -3,29 +3,25 @@ package utilidades;
 public class Circulo {
 	private double rad;
 
-	public Circulo(double radio)
-	    	{
-	        this.rad = radio;
+	public Circulo(double radio){
+		this.rad = radio;
 	}
 
-	    	public void imprimir()
-	    	{
-	        String color = "rojo";
-	        System.out.println("Di�metro: " + 2*rad);
-	        System.out.println("Color: " + color);
-	        double area = 2 * 3.1416 * rad * rad;
-	        System.out.println(area);
+	public void imprimir(){
+		String color = "rojo";
+	    System.out.println("Di�metro: " + 2*rad);
+	    System.out.println("Color: " + color);
+	    double area = 2 * 3.1416 * rad * rad;
+	    System.out.println(area);
 	}
 
-	public boolean esIgual (Circulo otro, boolean conDecimales)
-	{
-	        double radio1 = this.rad;
-	        double radio2 = otro.getRad();
-	        if (conDecimales)
-	        {
-	            if (radio1 == radio2) 
+	public boolean esIgual (Circulo otro, boolean conDecimales){
+		double radio1 = this.rad;
+	    double radio2 = otro.getRad();
+	    	if (conDecimales){
+	           if (radio1 == radio2) 
 	              return true;
-	            else
+	           else
 	              return false;
 	        }
 	        else
